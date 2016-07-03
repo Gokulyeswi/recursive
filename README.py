4caaab8e826cbf35e61344612f22964323c57db5
Gokula Krishnan <gokulyeswanth@gmail.com>
2016-07-03 22:16:27 +0530

    finish

---
+++ README.py
@@ -0,0 +1,20 @@
+# recursive
+def main():
+    a=[1,2,3,4,5]
+    b=[0,0,0,0,0]
+    n=len(l)
+def rev():
+	i=0
+        j= n-1
+	while(i<n):
+		if(i==n):
+			return 0;
+		else :
+			b[i]=a[j]
+			i=i+1
+			j=j-1
+rev()
+print (b)
+
+if __name__ == '__main__':
+    main()
